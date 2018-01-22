c37acc2b8c6a9b206c6fc78c143b25b999cf4442
Howard Huang <ccool2486@gmail.com>
2018-01-22 17:48:00 +0800

    linter setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,82 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			appName: 'Budget APP',
+			budgetCategories: [
+				{
+					key: 1,
+					title: '零用錢',
+					balance: 2000
+				},
+				{
+					key: 2,
+					title: 'Love',
+					balance: 3000
+				},
+				{
+					key: 3,
+					title: '家人',
+					balance: 500
+				},
+			]
+		}
+	}
+
+  
+
+	render() {
+		console.log('yo')
+		return (
+			<div className="App">
+				<MyfirstClassComponent/>
+				<MyFirstFunctionalComponent/>
+				<BudgetCategories budget={this.state.budgetCategories}/>
+				<BudgetCategories2 budget={this.state.budgetCategories}/>
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+				</p>
+			</div>
+		)
+	}
 }
 
-export default App;
+class MyfirstClassComponent extends Component {
+	render() {
+		return (
+			<p>This is from a Class Component</p>
+		)
+	}
+}
+
+const MyFirstFunctionalComponent = () => (
+	<p>Shout from Functional Componet</p> 
+)
+
+class BudgetCategories extends Component {
+	render() {
+		return (
+			this.props.budget.map(elem => {
+				return <p key={elem.key}>類別標題： {elem.title} / Balance: {elem.balance}</p>
+			})
+  
+		)
+	}
+}
+
+const BudgetCategories2 = (props) => ( 
+	props.budget.map(elem => <p key={elem.key}>{elem.title} / Balance: {elem.balance}</p>) 
+)
+
+
+
+export default App
