141314cc4814ab1bf9ae414d0d7b7ae52f6d5423
Howard Huang <ccool2486@gmail.com>
2018-01-23 09:53:11 +0800

    prettier

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {init as firebaseInit} from './javascripts/firebase'
+import { init as firebaseInit } from "./javascripts/firebase";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		firebaseInit()
-		//this.tick = this.tick.bind(this);
-		this.state = {
-			appName: "Budget APP",
-			budgetCounts: 3,
-			budgetCategories: [
-				{
-					key: 1,
-					title: "零用錢",
-					balance: 2000
-				},
-				{
-					key: 2,
-					title: "Love",
-					balance: 3000
-				},
-				{
-					key: 3,
-					title: "家人",
-					balance: 500
-				}
-			]
-		};
-	}
-
-	tick = () => {
-    this.setState({ budgetCounts: this.state.budgetCounts + 1 });
+  constructor(props) {
+    super(props);
+    firebaseInit();
+    //this.tick = this.tick.bind(this);
+    this.state = {
+      appName: "Budget APP",
+      budgetCounts: 3,
+      budgetCategories: [
+        {
+          key: 1,
+          title: "零用錢",
+          balance: 2000
+        },
+        {
+          key: 2,
+          title: "Love",
+          balance: 3000
+        },
+        {
+          key: 3,
+          title: "家人",
+          balance: 500
+        }
+      ]
+    };
   }
+
+  tick = () => {
+    this.setState({ budgetCounts: this.state.budgetCounts + 1 });
+  };
   // componentDidMount 為 component 生命週期中階段 component 已插入節點的階段，通常一些非同步操作都會放置在這個階段。這便是每1秒鐘會去呼叫 tick 方法
   componentDidMount() {
     this.interval = setInterval(this.tick, 1000);
@@ -43,49 +43,48 @@ class App extends Component {
     clearInterval(this.interval);
   }
 
-	render() {
-		return (
-			<div className="App">
-				<MyfirstClassComponent />
-				<MyFirstFunctionalComponent />
-				<BudgetCategories budget={this.state.budgetCategories} />
-				<BudgetCategories2 budget={this.state.budgetCategories} />
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">預算數量： {this.state.budgetCounts}</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <MyfirstClassComponent />
+        <MyFirstFunctionalComponent />
+        <BudgetCategories budget={this.state.budgetCategories} />
+        <BudgetCategories2 budget={this.state.budgetCategories} />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">預算數量： {this.state.budgetCounts}</p>
+      </div>
+    );
+  }
 }
 
 class MyfirstClassComponent extends Component {
-	render() {
-		return <p>This is from a Class Component</p>;
-	}
+  render() {
+    return <p>This is from a Class Component</p>;
+  }
 }
 
 const MyFirstFunctionalComponent = () => <p>Shout from Functional Componet</p>;
 
 class BudgetCategories extends Component {
-  
-	render() {
-		return this.props.budget.map(elem => {
-			return (
-				<p key={elem.key}>
+  render() {
+    return this.props.budget.map(elem => {
+      return (
+        <p key={elem.key}>
           類別標題： {elem.title} / Balance: {elem.balance}
-				</p>
-			);
-		});
-	}
+        </p>
+      );
+    });
+  }
 }
 
 const BudgetCategories2 = props =>
-	props.budget.map(elem => (
-		<p key={elem.key}>
-			{elem.title} / Balance: {elem.balance}
-		</p>
-	));
+  props.budget.map(elem => (
+    <p key={elem.key}>
+      {elem.title} / Balance: {elem.balance}
+    </p>
+  ));
 
 export default App;
